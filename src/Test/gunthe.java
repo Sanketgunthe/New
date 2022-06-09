1a719156d5aada7bce65b939ecb53fa6fb8dc987
Sanketgunthe <sanketgunthe06@gmail.com>
2022-06-09 21:03:04 +0530

    by other usr

--- src/Test/gunthe.java
+++ src/Test/gunthe.java
@@ -5,6 +5,14 @@ public class gunthe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("newly added ");
+		System.out.println("newly added ");
+		
+		System.out.println("newly added ");
+		System.out.println("newly added ");
+		System.out.println("newly added ");
+		
+		System.out.println("newly added ");
+		System.out.println("newly added ");
 	}
 
 }
